6f1eea8dbd0cf6b83b395546e5b2c87d0a480f8c
ABHISHEKJaiswar2363287362735682 <jaiswarabhishek12@gmail.com>
2025-07-08 08:20:58 +0530

    Update day1_tasks.py

--- DAY 1/day1_tasks.py
+++ DAY 1/day1_tasks.py
@@ -1,4 +1,4 @@
-#TASK - 1
+# Task 1: List & Dictionary Practice
 import math
 nums = [2,4,6,8,10]
 squares = []
@@ -12,8 +12,7 @@ for i in nums:
     dictionary[i] = i**2
 print(dictionary)
 
-#TASK - 2(String Manipulation)
-
+# Task 2: String Manipulation
 def count_vowel(s):
     count = 0   
     for char in s:
@@ -22,27 +21,24 @@ def count_vowel(s):
     return count
 print(count_vowel("Artificial Intelligence"))
 
-#TASK - 3
-
+# Task 3: File I/O Basic
 file = open("sample.txt", 'r')
 content = file.read()
 print(content)
 file.close()
     
-# TASK - 4
+# Task 4: Array Creation
 import numpy as np
-
 a = np.arange(10)
 print(a)
 print(a.shape)
 print(a.dtype)
 
-#TASK - 5
+# Task 5: Reshaping Practice
 b = np.array([1,2,3,4,5,6,7,8,9,10,11,12])
 print(b.reshape(3,4))
 
-# TASK - 6
-
+# Task 6: Array Operations
 c = np.random.randint(1,10, size = (3,3))
 d = np.random.randint(1,10, size = (3,3))
 print(c)
